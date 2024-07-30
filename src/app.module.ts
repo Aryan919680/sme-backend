160cc87bc27ef7af4bf872fb9e2b2ba2c58ad602
Aryan Malpotra <aryan.malpotra@originscale.io>
2024-07-30 18:09:14 +0530

    issues

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
-
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: PGHOST,
+      database: PGDATABASE,
+      username: PGUSER,
+      password: PGPASSWORD,       
       port: 5432,
-      username: 'postgres',
-      password: 'aryan',
-      database: 'postgres',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
+      ssl: true,
     }),
     UserModule,
   ],
@@ -21,3 +22,21 @@ import { UserModule } from './user/user.module';
   providers: [],
 })
 export class AppModule {}
+// import { Module } from '@nestjs/common';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { UserModule } from './user/user.module';
+
+// @Module({
+//   imports: [
+//     TypeOrmModule.forRoot({
+//       type: 'postgres',
+//       url: process.env.SUPABASE_DB_URL, // Use the connection string from the .env file
+//       entities: [__dirname + '/**/*.entity{.ts,.js}'],
+//       synchronize: true, // Set to false in production to avoid unintended schema changes
+//     }),
+//     UserModule,
+//   ],
+//   controllers: [],
+//   providers: [],
+// })
+// export class AppModule {}
